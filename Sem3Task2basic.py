6f0a0aec92ae75cec262ba792fe23427a8fcf84f
Julia <juliaf92@mail.ru>
2023-03-02 18:20:40 +0300

    added changes

--- Sem3Task2basic.py
+++ Sem3Task2basic.py
@@ -10,12 +10,12 @@ my_list = [7, 5, 3, 3, 2]
 print(my_list)
 new_elem = int(input('Введите новый элемент рейтинга: '))
 if new_elem <= my_list[4]:
-    my_list. append(new_elem)
+    my_list.append(new_elem)
     print(my_list)
 elif new_elem >= my_list[0]:
-    my_list. insert(0, new_elem)
+    my_list.insert(0, new_elem)
     print(my_list)
 elif new_elem < 7 and new_elem > 2:
-    my_list. append(new_elem)
-    my_list. sort(reverse=True)
+    my_list.append(new_elem)
+    my_list.sort(reverse=True)
     print(my_list)
