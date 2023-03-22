eac914250bd04f4a3fbdf7c94fa1cf14ea27cbbb
Julia <juliaf92@mail.ru>
2023-03-22 10:18:05 +0300

    Added a solutions for tasks by semenar9

--- Sem9Task2.py
+++ Sem9Task2.py
@@ -1,4 +1,18 @@
 '''
 Реализовать метакласс, позволяющий создавать всегда один и тот же объект класса
 '''
+class DocMeta(type):
+    def __init__(self, *args, **kwargs):
+        self.__instance = None
 
+    def __call__(self, *args, **kwargs):
+        if self.__instance is None:
+            self.__instance = super().__call__()
+        return self.__instance
+class MyClass(metaclass=DocMeta):
+    pass
+
+obj_1 = MyClass()
+obj_2 = MyClass()
+obj_3 = MyClass()
+print(obj_1 is obj_3)
